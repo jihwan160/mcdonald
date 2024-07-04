305a23c525da082bbecc15344509574535048be2
jihwan160 <dark160@naver.com>
2024-07-04 15:54:55 +0900

    content 추가

--- src/Pages/Home/Main.js
+++ src/Pages/Home/Main.js
@@ -57,6 +57,64 @@ const Background = styled.div`
 `;
 
 
+const ContentCover = styled.div`
+    padding: 30px 0 60px;
+`;
+
+const Content = styled.div`
+    max-width: 1168px;
+    margin: 0 auto;
+
+    & .maintit {
+        font-size: 40px;
+        font-weight: 600;
+        line-height: 1;
+        margin: 0 0 60px;
+        color: #292929;
+    }
+
+    & .cardListcol {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 35px;
+        & .cardList {
+            width: 366px;
+            height: 438px;
+            margin: 0 0 35px;
+            border-radius: 10px;
+            background-color: #fff;
+            box-shadow: 0 3px 4px rgba(0,0,0,.1);
+            overflow: hidden;
+            cursor: pointer;
+
+            & .con {
+                padding: 24px 30px;
+                font-size: 17px;
+                color: #292929;
+                font-weight: bold;
+                line-height: 1.7;
+            }
+        }
+    }
+
+    
+    & .more {
+        margin-top: 80px;
+        text-align: center;
+    }
+`;
+
+const BtnMore = styled.div`
+    background-image: url(${process.env.PUBLIC_URL}img/main/btn_more.png);
+    background-position: 0 0;
+    background-repeat: no-repeat;
+        width: 90px;
+        height: 90px;
+        display: inline-block;
+        overflow: hidden;
+        cursor: pointer;
+`;
+
 const Main = () => {
 
     const slidelist = [
@@ -177,10 +235,78 @@ const Main = () => {
         )
     }
 
+    const MainContent = () => {
+        return(
+            <ContentCover>
+                <Content>
+                    <h2 className='maintit'>McDonald's LIVE</h2>
+                    <ul className='cardListcol'>
+                        <li className='cardList'>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}img/main/cardlist1.png`} alt='cardlist1'/>
+                            </div>
+                            <div className='con'>
+                                버거 세트 구매시 <br />
+                                사이드 무료 교환 안내
+                            </div>
+                        </li>
+                        <li className='cardList'>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}img/main/cardlist2.jpg`} alt='cardlist2'/>
+                            </div>
+                            <div className='con'>
+                                NEW 맥스파이시 상하이를 만나보세요!
+                            </div>
+                        </li>
+                        <li className='cardList'>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}img/main/cardlist3.jpg`} alt='cardlist3'/>
+                            </div>
+                            <div className='con'>
+                                쌀가루로 더 바삭해진 NEW 맥스파이시!
+                            </div>
+                        </li>
+                        <li className='cardList'>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}img/main/cardlist4.jpg`} alt='cardlist4'/>
+                            </div>
+                            <div className='con'>
+                                빠삭하게 빠져드는 맛,<br />
+                                맥크리스피!
+                            </div>
+                        </li>
+                        <li className='cardList'>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}img/main/cardlist5.jpg`} alt='cardlist5'/>
+                            </div>
+                            <div className='con'>
+                                가성비 간식 맛집<br />
+                                맥도날드 해피스낵!
+                            </div>
+                        </li>
+                        <li className='cardList'>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}img/main/cardlist6.jpg`} alt='cardlist6'/>
+                            </div>
+                            <div className='con'>
+                                겉은 바삭! 속은 쫄깃<br />
+                                여름엔 역시 맥윙!
+                            </div>
+                        </li>
+                    </ul>
+                    <div className='more'>
+                        <BtnMore></BtnMore>
+                    </div>
+                </Content>
+            </ContentCover>
+        )
+    }
+
 
     return (
         <>
             <Slide />
+            <MainContent />
         </>
     )
 }
