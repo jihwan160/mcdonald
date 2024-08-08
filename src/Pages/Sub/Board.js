036f67186d2b0384beea4c22013a6f49a6ee195c
jihwan160 <dark160@naver.com>
2024-08-08 18:29:54 +0900

    board 추가

--- src/Pages/Sub/Board.js
+++ src/Pages/Sub/Board.js
@@ -64,50 +64,54 @@ const VisualImg = styled.div`
     }
 `;
 
-const ContentCover = styled.div`
+const BoardArea = styled.div`
     padding: 30px 0 60px;
+
 `;
 
-const Content = styled.div`
+const BoardAreaCover = styled.div`
     max-width: 1168px;
     margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-    & .maintit {
-        font-size: 40px;
-        font-weight: 600;
-        line-height: 1;
-        margin: 0 0 60px;
+    & .boardCnt {
         color: #292929;
-    }
-
-    & .cardListcol {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        gap: 35px;
-        & .cardList {
-            width: 366px;
-            height: 438px;
-            margin: 0 0 35px;
-            border-radius: 10px;
-            background-color: #fff;
-            box-shadow: 0 3px 4px rgba(0,0,0,.1);
-            overflow: hidden;
-            cursor: pointer;
-
-            & .con {
-                padding: 24px 30px;
-                font-size: 17px;
-                color: #292929;
-                font-weight: bold;
-                line-height: 1.7;
-            }
+        font-weight: 500;
+        & span {
+            color: #da0000;
         }
     }
-    
-    & .more {
-        margin-top: 45px;
-        text-align: center;
+
+    & .search {
+
+        display: flex;
+        align-items: center;
+        & input {
+            width: 300px;
+            height: 50px;
+            padding: 0 28px;
+            border: 1px solid #d1d1d1;
+            border-radius: 70px;
+            background: #fff;
+            box-sizing: border-box;
+            font-size: 16px;
+            color: #292929;
+            font-weight: 600;
+            letter-spacing: -1px;
+        }
     }
+`;
+
+const SearchBtn = styled.button`
+    width: 50px;
+    height: 50px;
+    background-image: url(${process.env.PUBLIC_URL}/img/news/btn_search.png);
+    background-position: 0 0;
+    background-repeat: no-repeat;
+    margin-left: 8px;
+    cursor: pointer;
 `;
 
 const Board = () => {
@@ -120,11 +124,11 @@ const Board = () => {
             <>
                 <VisualImg>
                     <div className='bvcon'>
-                        <h2>이달의 해피밀</h2>
+                        <h2>새로운 소식</h2>
                         <ul>
                             <li onClick={()=>{navigate('/')}}>Home</li>
                             <li onClick={()=>{navigate('/whats')}}>What's New </li>
-                            <li onClick={()=>{navigate('/whats')}}>이달의 해피밀</li>
+                            <li onClick={()=>{navigate('/whats')}}>새로운 소식</li>
                         </ul>
                     </div>
                 </VisualImg>
@@ -134,32 +138,26 @@ const Board = () => {
 
     const BoardWrite = () => {
 
-        const initialList = [
-            {
-                img : `${process.env.PUBLIC_URL}img/news/mhm1.png`,
-                alt : 'mhm1',
-                con : '슈퍼배드4!  영화속 주인공 미니언즈를  맥도날드 해피밀 토이로 만나보세요.',
-            },
-        ]
+        // const initialList = [
+        //     {
+        //         img : `${process.env.PUBLIC_URL}img/news/mhm1.png`,
+        //         alt : 'mhm1',
+        //         con : '슈퍼배드4!  영화속 주인공 미니언즈를  맥도날드 해피밀 토이로 만나보세요.',
+        //     },
+        // ]
         
         return(
-            <ContentCover>
-                <Content>
-                    <h2 className='maintit'>McDonald's LIVE</h2>
-                    <ul className='cardListcol'>
-                        {initialList.map((i, index) => (
-                            <li className='cardList' key={index}>
-                                <div>
-                                    <img src={i.img} alt={i.alt}/>
-                                </div>
-                                <div className='con'>
-                                    {i.con}
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </Content>
-            </ContentCover>
+            <div>
+                <BoardArea>
+                    <BoardAreaCover>
+                        <p className='boardCnt'>총 <span>0건</span>의 게시글이 있습니다.</p>
+                        <div className='search'>
+                            <input type='text' placeholder='검색어를 입력하세요' />
+                            <SearchBtn></SearchBtn>
+                        </div>
+                    </BoardAreaCover>
+                </BoardArea>
+            </div>
         )
     }
 
