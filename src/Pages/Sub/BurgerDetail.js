ea9b99d3e1e296654487ab33e888811028deb4ea
jihwan160 <dark160@naver.com>
2024-08-16 18:16:58 +0900

    디테일 추가

--- src/Pages/Sub/BurgerDetail.js
+++ src/Pages/Sub/BurgerDetail.js
@@ -153,24 +153,44 @@ const BurgerDetail = () => {
             time : '*판매 시간: 오전 10시 30분 ~ 새벽 4시'
         },  
         {
+            name : '토마토 치즈 비프 버거',
+            eng : 'Tomato Cheese Beef Burger',
             img : `${process.env.PUBLIC_URL}img/burger/detail/detail3.png`,
             alt : 'detail3',
+            con : '신선한 토마토와 고소한 치즈버거의 조화',
+            time : '*판매 시간: 오전 10시 30분 ~ 새벽 4시'
         },
         {
+            name : '더블 쿼터파운더® 치즈',
+            eng : 'Double Quarter Pounder® with Cheese',
             img : `${process.env.PUBLIC_URL}img/burger/detail/detail4.png`,
             alt : 'detail4',
+            con : '좋아하는건 더 많이 즐기시라고,\n두 배 이상 커진 1/4 파운드비프 패티가 두 장 (226그램)!\n육즙이 풍부한 고기 맛을\n그대로 살린 100% 순 쇠고기 패티 두 장과\n치즈 두 장이 입안 가득 완벽하게 조화되는 놀라운 맛',
+            time : '*판매 시간: 오전 10시 30분 ~ 새벽 4시'
         },
         {
+            name : '쿼터파운더® 치즈',
+            eng : 'Quarter Pounder® with Cheese',
             img : `${process.env.PUBLIC_URL}img/burger/detail/detail5.png`,
             alt : 'detail5',
+            con : '쿼터파운더라는 이름에서 알 수 있듯이\n두 배 이상 커진 1/4파운드(113그램) 비프와\n부드러운 치즈 두 장의 환상궁합!\n두툼한 순 쇠고기 패티와 신선한 치즈의\n풍부한 맛으로 세계적으로 사랑받고 있는\n맥도날드의 대표적인 프리미엄 버거',
+            time : '*판매 시간: 오전 10시 30분 ~ 새벽 4시'
         },
         {
+            name : '빅맥®',
+            eng : 'Big Mac®',
             img : `${process.env.PUBLIC_URL}img/burger/detail/detail6.png`,
             alt : 'detail6',
+            con : '100% 순 쇠고기 패티 두 장에 빅맥®만의 특별한 소스.\n입안에서 살살 녹는 치즈와 신선한 양상추, 양파, 그리고 피클까지.\n50년 넘게 전 세계인의 입맛을 사로 잡은 버거의 대명사.',
+            time : '*판매 시간: 오전 10시 30분 ~ 새벽 4시'
         },
         {
+            name : '맥크리스피® 디럭스 버거',
+            eng : 'McCrispy® Deluxe Burger',
             img : `${process.env.PUBLIC_URL}img/burger/detail/detail7.png`,
             alt : 'detail7',
+            con : '100% 통닭다리살 겉바속촉 케이준 치킨 패티,\n촉촉한 포테이토 브리오쉬 번, 스페셜 스모키 소스가\n선사하는 놀랍도록 새로운 맛의 치킨 버거!',
+            time : '*판매 시간: 오전 10시 30분 ~ 새벽 4시'
         },
     ]
 
